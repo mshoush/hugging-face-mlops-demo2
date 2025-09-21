6ff1771f27145bc3048eb12362f1a534f27f0ba9
Mahmoud shoush <mahmoud.kamel104@gmail.com>
2025-09-21 09:55:47 +0000

    edit app.py and req.txt

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
+import torch
 from transformers import pipeline
 import gradio as gr
 
-
-model = pipeline(
-    "summarization",
-)
+# load summarization model
+model = pipeline("summarization", model="facebook/bart-large-cnn")  # specify model
 
 def predict(prompt):
     summary = model(prompt)[0]["summary_text"]
     return summary
 
-
-# create an interface for the model
-with gr.Interface(predict, "textbox", "text") as interface:
+# create Gradio interface
+with gr.Interface(fn=predict, inputs="textbox", outputs="text") as interface:
     interface.launch()
+
+
+
+# from transformers import pipeline
+# import gradio as gr
+
+
+# model = pipeline(
+#     "summarization",
+# )
+
+# def predict(prompt):
+#     summary = model(prompt)[0]["summary_text"]
+#     return summary
+
+
+# # create an interface for the model
+# with gr.Interface(predict, "textbox", "text") as interface:
+#     interface.launch()
